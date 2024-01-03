75949515d266703b2d7871c6ce0861432d793556
kylesock <kyle161001@gmail.com>
2024-01-03 00:27:16 +0000

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/cppRoadmap.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppRoadmap.dir/main.cpp.o"
+  "CMakeFiles/cppRoadmap.dir/main.cpp.o.d"
+  "cppRoadmap"
+  "cppRoadmap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppRoadmap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
